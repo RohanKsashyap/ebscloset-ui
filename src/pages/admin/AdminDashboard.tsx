b5e4e7e26716b89a192538828c8c94bdb0c6326b
RohanKashyap <roy282227@gmail.com>
2026-05-07 11:55:21 +0530

    commit

--- src/pages/admin/AdminDashboard.tsx
+++ src/pages/admin/AdminDashboard.tsx
@@ -1029,6 +1029,11 @@ export default function AdminDashboard() {
               </div>
             )}
 
+
+
+{/* customer tab starts here */}
+
+
             {tab === 'customers' && (
               <div className="space-y-8 animate-in fade-in slide-in-from-bottom-4 duration-700">
                 <CustomersManagement users={users} />
